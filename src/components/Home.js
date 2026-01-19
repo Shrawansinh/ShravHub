a98ec89f8be0348a8aafd4939cc8c63778c33b6e
Shrawansinh prankada <147142619+Shrawansinh@users.noreply.github.com>
2026-01-19 12:57:32 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,54 @@
-import React, { useRef }  from 'react'
-import '../App.css';
+import React from 'react'
+import '../App.css'
 import businessImage from '../images/Business.png'
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
 export default function Home(props) {
   return (
     <>
-    <div className={`container text-${props.mode} text-center`}>
-      <h1 className='header'>Every tool you need to work with Text, Images and PDFs in one place</h1>
-      <h3 className='mt-3'>Every tool you need to use PDFs , Text And  Images , at your fingertips. All are 100% FREE and easy to use! Merge, split, compress, convert, rotate, unlock and watermark PDFs with just a few clicks.</h3>
-      <button type="button" className="btn btn-dark btn-lg mt-5">All</button>
-      <Link to="/textform">
-      <button type="button" className="btn btn-outline-dark btn-lg ms-5 mt-5">Text</button>
-    </Link>
-    <button type="button" className="btn btn-outline-dark btn-lg ms-5 mt-5">Images</button>
-      <button type="button" className="btn btn-outline-dark btn-lg ms-5 mt-5">Pdf's</button>
-    </div>
-    <div className="container images">
-        <img src={businessImage} className="rounded mx-auto d-block mt-5 po" alt="..." width="400"></img>
-    </div>
+      <div className={`container text-${props.mode} text-center`}>
+        <h1 className='header'>
+          Every tool you need to work with Text, Images and PDFs in one place
+        </h1>
 
+        <h3 className='mt-3'>
+          Every tool you need to use PDFs, Text and Images, at your fingertips.
+          All are 100% FREE and easy to use!
+        </h3>
 
+        <button className="btn btn-dark btn-lg mt-5">All</button>
+
+        <Link
+          to="/textform"
+          className="btn btn-outline-dark btn-lg ms-5 mt-5"
+        >
+          Text
+        </Link>
+
+        <Link
+          to="/images"
+          className="btn btn-outline-dark btn-lg ms-5 mt-5"
+        >
+          Images
+        </Link>
+
+        <Link
+          to="/pdf"
+          className="btn btn-outline-dark btn-lg ms-5 mt-5"
+        >
+          Pdf's
+        </Link>
+      </div>
+
+      <div className="container images">
+        <img
+          src={businessImage}
+          className="rounded mx-auto d-block mt-5 po"
+          alt="Business"
+          width="400"
+        />
+      </div>
     </>
   )
 }
+
